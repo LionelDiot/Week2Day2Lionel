5f089bf05d39b37b41633e877ae5d0f5fb7b0f5a
lionel <diot.lionel2@gmail.com>
2023-01-18 04:02:54 +0100

    ajout de condition entre 1 et 25 exo 15 16 comme dans lexo 17

--- exo_15.rb
+++ exo_15.rb
@@ -1,6 +1,10 @@
-puts " Salut, bienvenue dans ma pyramide, combien d'etages veux tu"
-print ">"
+nb_etage=0
+
+while !((nb_etage>=1) && (nb_etage <=25)) do
+  puts " Salut, bienvenue dans ma pyramide, combien d'etages (entre 1 et 25) veux tu"
+print "> "
 nb_etage=gets.to_i 
+end 
 puts " et voici la pyramide"
 i=1
 while i<=nb_etage do
